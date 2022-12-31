ae3549090273ad8a1f11fe8d76678f27c4c11d2c
Sonu Kumar <sonuhits047@gmail.com>
2022-12-31 17:05:23 +0530

    first commit

---
+++ ftweb25-variable/index.js
@@ -0,0 +1,9 @@
+let a= 10;
+
+console.log(a);
+
+let name= "Sonu Kumar"
+
+console.log(name);
+console.log(typeof(a));
+console.log(typeof(name))
---
+++ ftweb25-variable/problem2.js
@@ -0,0 +1,7 @@
+let name;
+name= "Sonu Kumar";
+console.log(name);
+name= "Akhilesh Mahto";
+console.log(name);
+name= "Gita Devi";
+console.log(name);
